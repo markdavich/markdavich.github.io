f6abb417757e1affe5c99a8b9a4b59afc0cc0d95
Mark Davich <markdavich@gmail.com>
2019-08-12 08:34:09 -0600

    General layout

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-window.addEventListener('scroll', () => {
-  let parent = document.getElementById('parallax-images');
-  let children = parent.getElementsByClassName('parallax-img');
-  for (let i = 0; i < children.length; i++) {
-    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
-  }
-}, false)
+window.addEventListener('scroll', function (e) {
+
+  // This is the only line needed for parallax
+  document.getElementById('p-background').style.top = `-${window.scrollY}px`;
+
+  return;
+
+  // The information section
+  let scrollY = window.scrollY;
+  let scrollHeight = document.body.scrollHeight;
+  let innerHeight = window.innerHeight;
+  let pageYOffset = window.pageYOffset;
+  let bodyH = document.body.clientHeight;
+  let bodySH = document.body.scrollHeight;
+
+  // This was a bit weird...
+  let body = document.body;
+  let html = document.documentElement;
+  let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+  document.getElementById('p-content').innerText = `Just some info:
+    window.scrollY = ${Math.round(scrollY)}
+    window.innerHeight = ${innerHeight}
+    window.pageYOffset = ${Math.round(pageYOffset)}
+    body.scrollHeight = ${scrollHeight}
+    body.clientHeight = ${bodyH}
+    body.scrollHeight = ${bodySH}
+
+    after a while...
+    Page Content Height = ${height}
+    Scroll = ${Math.round(scrollY / (height - innerHeight) * 100)}%
+  `;
+});
